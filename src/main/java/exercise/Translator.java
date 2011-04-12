588e4c7f4c525738d06402755506614884dc6fa7
Tony Baines <tony.baines.1@gmail.com>
2011-04-12 21:06:34 +0100

    Tidied up

--- src/main/java/exercise/Translator.java
+++ src/main/java/exercise/Translator.java
@@ -11,57 +11,60 @@ public class Translator {
     private static final String[] DIGITS = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     private static final String[] TENS = {"NaN", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     private static final String[] TEENS = {"NaN", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static final String EMPTY_STRING = "";
+    private static final String SPACE = " ";
 
-    public String translate(int i) {
+    public String translate(final int i) {
         if (i < TEN)
             return translateSingleDigit(i);
-        else if(i < ONE_HUNDRED)
+        else if (i < ONE_HUNDRED)
             return translateTens(i);
-        else if(i < ONE_THOUSAND)
-            return translateHundreds(i);
-        else if(i < ONE_MILLION)
-            return translateThousands(i);
-        else if(i < ONE_BILLION)
-            return translateMillions(i);
+        else if (i < ONE_THOUSAND)
+            return translate(i, ONE_HUNDRED, "hundred");
+        else if (i < ONE_MILLION)
+            return translate(i, ONE_THOUSAND, "thousand");
+        else if (i < ONE_BILLION)
+            return translate(i, ONE_MILLION, "million");
         else
             throw new IllegalArgumentException("I don't understand numbers bigger than " + MAX_VALUE);
     }
 
-    private String translateSingleDigit(int i) {
+    private String translateSingleDigit(final int i) {
         return DIGITS[i];
     }
 
-    private String translateTens(int i) {
-        if (i% TEN == 0)
-            return TENS[i/ TEN];
+    private String translateTens(final int i) {
+        int scaledDown = scaledDown(i, TEN);
+        int remainder = remainder(i, TEN);
+        if (remainder == 0)
+            return TENS[scaledDown];
         else if (i > 20)
-            return translate(i - i% TEN)+" "+translate(i% TEN);
+            return translate(i - remainder) + SPACE + translate(remainder);
         else
-            return TEENS[i- TEN];
+            return TEENS[i - TEN];
     }
 
-    private String translateHundreds(int i) {
-        return translate(i/ONE_HUNDRED, i%ONE_HUNDRED, "hundred");
+    private String translate(final int i, final int scale, final String separator) {
+        int scaledDown = scaledDown(i, scale);
+        int remainder = remainder(i, scale);
+        return translate(scaledDown) + SPACE + separator + translateRemainder(remainder);
     }
 
-    private String translateThousands(int i) {
-        return translate(i/ONE_THOUSAND, i%ONE_THOUSAND, "thousand");
+    private String translateRemainder(final int remainder) {
+        if (remainder > 0)
+            return lessThanOneHundredSpecialCase(remainder) + SPACE + translate(remainder);
+        else
+            return EMPTY_STRING;
     }
 
-    private String translateMillions(int i) {
-        return translate(i/ONE_MILLION, i%ONE_MILLION, "million");
+    private String lessThanOneHundredSpecialCase(final int remainder) {
+        if (remainder < ONE_HUNDRED)
+            return " and";
+        else
+            return EMPTY_STRING;
     }
 
-    private String translate(int roundedDown, int remainder, String separator) {
-        StringBuffer retVal = new StringBuffer(translate(roundedDown)+" "+separator);
-
-        if (remainder > 0) {
-            if (remainder < ONE_HUNDRED) { retVal.append(" and"); }
-            retVal.append(" ");
-            retVal.append(translate(remainder));
-        }
-
-        return retVal.toString();
-    }
+    private int scaledDown(final int i, final int scale) { return i/scale; }
+    private int remainder(final int i, final int scale) { return i%scale; }
 
 }
