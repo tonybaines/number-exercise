ab27de506927e795d48bdc59f8b07057bec9b2a5
Tony Baines <tony.baines@bt.com>
2011-04-12 13:23:27 +0100

    Refactored the test o remove duplication

--- src/test/java/exercise/TranslatorTest.java
+++ src/test/java/exercise/TranslatorTest.java
@@ -7,45 +7,31 @@ public class TranslatorTest {
     private Translator translator = new Translator();
 
     @Test public void shouldTranslateASingleDigit() {
-        assertEquals("zero", translator.translate(0));
-        assertEquals("one", translator.translate(1));
-        assertEquals("two", translator.translate(2));
-        assertEquals("three", translator.translate(3));
-        assertEquals("four", translator.translate(4));
-        assertEquals("five", translator.translate(5));
-        assertEquals("six", translator.translate(6));
-        assertEquals("seven", translator.translate(7));
-        assertEquals("eight", translator.translate(8));
-        assertEquals("nine", translator.translate(9));
+        shouldTranslate(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,},
+                     new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"});
     }
 
     @Test public void shouldTranslateMultiplesOfTenUpToNinety() {
-        assertEquals("ten", translator.translate(10));
-        assertEquals("twenty", translator.translate(20));
-        assertEquals("thirty", translator.translate(30));
-        assertEquals("forty", translator.translate(40));
-        assertEquals("fifty", translator.translate(50));
-        assertEquals("sixty", translator.translate(60));
-        assertEquals("seventy", translator.translate(70));
-        assertEquals("eighty", translator.translate(80));
-        assertEquals("ninety", translator.translate(90));
+        shouldTranslate(new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90,},
+                             new String[]{"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"});
     }
 
     @Test public void shouldTranslateTheTeens() {
-        assertEquals("eleven", translator.translate(11));
-        assertEquals("twelve", translator.translate(12));
-        assertEquals("thirteen", translator.translate(13));
-        assertEquals("fourteen", translator.translate(14));
-        assertEquals("fifteen", translator.translate(15));
-        assertEquals("sixteen", translator.translate(16));
-        assertEquals("seventeen", translator.translate(17));
-        assertEquals("eighteen", translator.translate(18));
-        assertEquals("nineteen", translator.translate(19));
+        shouldTranslate(new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19,},
+                     new String[]{"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"});
+
     }
 
     @Test public void shouldTranslateNumbersBetweenTwentyOneAndNinetyNine() {
-        assertEquals("twenty one", translator.translate(21));
-        assertEquals("thirty seven", translator.translate(37));
-        assertEquals("ninety nine", translator.translate(99));
+        shouldTranslate(new int[]{21, 37, 99},
+        new String[]{"twenty one", "thirty seven", "ninety nine"});
+    }
+
+
+
+    private void shouldTranslate(int[] from, String[] to) {
+        for (int i=0; i< from.length; i++) {
+            assertEquals(to[i], translator.translate(from[i]));
+        }
     }
 }
